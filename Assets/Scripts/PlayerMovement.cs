517e5ccfa10df6b4b3f87377dfc860638739bf56
Soul Evans <soulevans07@gmail.com>
2019-05-18 23:37:43 +0200

    initial commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+	private Transform _transform;
+	private Rigidbody2D _rigidbody;
+	private Vector3 move;
+
+	public float maxSpeed;
+
+	void Start() {
+		this._transform = GetComponent<Transform>();
+		this._rigidbody = GetComponent<Rigidbody2D>();
+	}
+
+	void Update() {
+		move.x = Input.GetAxis("Horizontal");
+		move.y = Input.GetAxis("Vertical");
+	}
+
+	private void FixedUpdate() {
+		this._rigidbody.velocity = move * maxSpeed;
+	}
+}

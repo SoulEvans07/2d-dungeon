41de61dd01a2a0a8fb4f22d4c3c42d5f802c6a54
Soul Evans <soulevans07@gmail.com>
2019-05-19 16:27:14 +0200

    added magic staff

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+	private Transform _transform;
+	private Staff attributes;
+	public float speed = 1f;
+
+	private void Start() {
+		this._transform = GetComponent<Transform>();
+	}
+
+	public void SetAttributes(Staff staff) {
+		this.attributes = staff;
+	}
+
+	private void Update() {
+		this._transform.position += this._transform.right * speed;
+	}
+
+	private void OnTriggerEnter2D(Collider2D other) {
+		Debug.Log(other.name);
+		if (other.CompareTag(attributes.target)) {
+			Health enemy = other.GetComponent<Health>();
+			enemy.TakeHit(attributes.damage);
+		}
+
+		if (other.CompareTag(attributes.target) || other.CompareTag("Obstacle")) {
+			Destroy(this.gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/Staff.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Staff : MonoBehaviour {
+	private Transform _transform;
+
+	public Transform castPos;
+	public GameObject projectile;
+	public float damage = 1f;
+	public string caster;
+	public string target;
+
+	public void Start() {
+		this._transform = GetComponent<Transform>();
+		SetOwner();
+	}
+
+	public void SetOwner() {
+		string ownerTag = this._transform.parent.tag;
+		switch (ownerTag) {
+			case "Player":
+				this.caster = "Player";
+				this.target = "Enemy";
+				break;
+			// case "Enemy":
+			default:
+				this.caster = "Enemy";
+				this.target = "Player";
+				break;
+		}
+	}
+
+	public void Cast() {
+		GameObject proj = Instantiate(projectile, castPos.position, _transform.rotation);
+		proj.GetComponent<Projectile>().SetAttributes(this);
+	}
+}

4fbf0990ccaf9050946454e2a2266725b1febc1b
Soul Evans <soulevans07@gmail.com>
2019-05-19 03:08:20 +0200

    added attack() and flip() to player

---
+++ Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour {
+	private static readonly int Swing = Animator.StringToHash("swing");
+	private Animator _animator;
+
+	public float attackSpeed = 1f;
+	private bool attack;
+	private float timer;
+
+	private void Start() {
+		this._animator = GetComponent<Animator>();
+		timer = 1f / attackSpeed;
+	}
+
+	private void Update() {
+		timer += Time.deltaTime;
+		attack = canAttack() && Input.GetButton("Fire1");
+	}
+
+	private void FixedUpdate() {
+		if (attack) {
+			this._animator.SetTrigger(Swing);
+			attack = false;
+			timer = 0;
+		}
+	}
+
+	private bool canAttack() {
+		return timer >= 1f / attackSpeed;
+	}
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,20 +4,45 @@ public class PlayerMovement : MonoBehaviour {
 	private Transform _transform;
 	private Rigidbody2D _rigidbody;
 	private Vector3 move;
+	private Vector3 mouse;
 
 	public float maxSpeed;
+	
+	public Camera _camera;
 
-	void Start() {
+	private void Start() {
 		this._transform = GetComponent<Transform>();
 		this._rigidbody = GetComponent<Rigidbody2D>();
 	}
 
-	void Update() {
+	private void Update() {
 		move.x = Input.GetAxis("Horizontal");
 		move.y = Input.GetAxis("Vertical");
+
+		Vector3 m2w = _camera.ScreenToWorldPoint(Input.mousePosition);
+		mouse.x = m2w.x;
+		mouse.y = m2w.y;
 	}
 
 	private void FixedUpdate() {
+		Move();
+		Flip();
+	}
+
+	private void Move() {
 		this._rigidbody.velocity = move * maxSpeed;
+	}
+
+	private void Flip() {
+		if (this._transform.localScale.x < 0 && mouse.x > this._transform.position.x || 
+		    this._transform.localScale.x > 0 && mouse.x < this._transform.position.x) {
+			FlipX();
+		}
+	}
+
+	private void FlipX() {
+		Vector3 scale = this._transform.localScale;
+		scale.x *= -1;
+		this._transform.localScale = scale;
 	}
 }
